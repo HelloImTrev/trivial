fc6a8198c305e580f08d3f3b39d2d4621d7b140e
Trevor Latimer <trevorplatimer@gmail.com>
2022-08-13 13:09:25 -0500

    added answer logic

--- src/Components/QuestionCards.js
+++ src/Components/QuestionCards.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
  export const QuestionCards = ({questions}) => {
+  const [currentQuestion, setCurrentQuestion] = useState(questions[0]);
+  const [answers, setAsnwers] = useState([]);
+
+  useEffect(() => {
+    setAsnwers(getAnswers(currentQuestion));
+  }, []);
+
+  console.log(answers);
+
+  const getAnswers = ( question ) => {
+    const allAnswers = []
+    const correctAnswer = question.correctAnswer;
+    const incorrectAnswers = question.incorrectAnswers;
+    
+    allAnswers.push(correctAnswer);
+    incorrectAnswers.forEach(answer => allAnswers.push(answer));
+
+    return allAnswers;
+  };
+
+
   return(
     <div>
-      {questions.map((q, index) => {
-        return(
-            <h3 key={index}>{q.question}</h3>
-        )
-      })}
+      <h2 className="question">{currentQuestion.question}</h2>
     </div>
   )
 }
